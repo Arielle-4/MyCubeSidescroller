52c704665ae81486566dbb8aead38dedacdad4bf
Arielle-4 <arielle-4@hotmail.com>
2014-10-27 13:42:10 +0100

    Generating a skyline 1
    - raw of cubes to construct the skyline
    -c# script called  SkylineManager
    - new empty object  named Managers
    - turning skyline cubes into prefab
    -start position variable
    -variable named numberOfCubes
    -private variable named nextPosition.
    - use of a loop for spawning the initial row of cubes
    - Start Position to (0, -1, 0)
    -Number of Objects to 10
    
    _ in the runner script
    
    - static variable named distanceTraveled to Runner

--- Assets/Runner/Runner.cs
+++ Assets/Runner/Runner.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Runner : MonoBehaviour {
 
+	public static float distanceTraveled;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,5 +14,6 @@ public class Runner : MonoBehaviour {
 	void Update () {
 	
 		transform.Translate(5f * Time.deltaTime, 0f, 0f);
+		distanceTraveled = transform.localPosition.x;
 	}
 }
---
+++ Assets/Skyline/SkylineManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SkylineManager : MonoBehaviour {
+
+	public Transform prefab;
+	public int numberOfObjects;
+	public Vector3 startPosition;
+	private Vector3 nextPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+		nextPosition = startPosition;
+		for (int i= 0;i<numberOfObjects;i++){
+			Transform o = ( Transform) Instantiate (prefab);
+			o.localPosition= nextPosition;
+			nextPosition.x += o.localScale.x;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
