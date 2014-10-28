7faec80bbca94f0d153e02a1d5dd7d8bc8868663
Arielle-4 <arielle-4@hotmail.com>
2014-10-28 15:33:19 +0100

    Generating a skyline 2
    -recycling skyline
    -randomly scale the cubes whenever they're placed or recycled.

--- Assets/Skyline/SkylineManager.cs
+++ Assets/Skyline/SkylineManager.cs
@@ -1,25 +1,48 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class SkylineManager : MonoBehaviour {
 
 	public Transform prefab;
 	public int numberOfObjects;
+	public float recycleOffset;
 	public Vector3 startPosition;
+
 	private Vector3 nextPosition;
+	private Queue<Transform> objectQueue;
 
 	// Use this for initialization
 	void Start () {
+
+		objectQueue = new Queue<Transform>(numberOfObjects);
+		for (int i = 0; i < numberOfObjects; i++) {
+			objectQueue.Enqueue((Transform)Instantiate(prefab));
+		}
 	
+		
 		nextPosition = startPosition;
 		for (int i= 0;i<numberOfObjects;i++){
-			Transform o = ( Transform) Instantiate (prefab);
-			o.localPosition= nextPosition;
-			nextPosition.x += o.localScale.x;
+			Recycle();
 		}
+
 	}
+	
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+		if (objectQueue.Peek().localPosition.x + recycleOffset < Runner.distanceTraveled) {
+			Recycle();
+
+		}
+
+		}
+
+	        private void Recycle () {
+			Transform o = objectQueue.Dequeue();
+			o.localPosition = nextPosition;
+			nextPosition.x += o.localScale.x;
+			objectQueue.Enqueue(o);
+		}
+
 }
