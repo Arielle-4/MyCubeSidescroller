94e4fc80c7df3120789599ef7eb84b9ff3e5a8d0
Arielle-4 <arielle-4@hotmail.com>
2014-11-01 19:29:57 +0100

    GUI and Game Start
    -create a GUI and a manager that uses it
    -Game Over Text
    - Instructions Text
    -Runner Text
    -reate a C# script named GUIManager in the Managers folder
    -Start method to our new manager and use it to disable gameOverText s
    -Update method that checks whether a jump button was pressed, and if so
    triggers the game-start event.
    -method to handle our game-start event:GameStart
    -nforming our event manager that it should call the GameStart method of
    our manager object, whenever the game-start event is triggered. adding
    our method to the event in the Start method.

---
+++ Assets/Managers/GUIManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+
+
+	public class GUIManager : MonoBehaviour {
+		
+		public GUIText gameOverText, instructionsText, runnerText;
+
+
+ void Start () {
+
+	GameEventManager.GameStart += GameStart;
+	gameOverText.enabled = false;
+}
+
+void Update () {
+	if(Input.GetButtonDown("Jump")){
+		GameEventManager.TriggerGameStart();
+	}
+}
+
+private void GameStart () {
+	gameOverText.enabled = false;
+	instructionsText.enabled = false;
+	runnerText.enabled = false;
+	enabled = false;
+}
+
+}
